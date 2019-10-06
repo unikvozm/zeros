8e1319b0a50a8ce09eff836f5f26a12ebd04b74d
Natalia Volkova <natalia.volkofff@gmail.com>
2019-10-06 18:16:29 +0500

    feat: solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  let zeros = 0,
+    two = 0,
+    five = 0;
+  let arr = expression.split("*");
+  for (let i = 0; i < arr.length; i++) {
+    if (/!!/.test(arr[i])) {
+      let num = parseInt(arr[i]);
+      if (num % 2 === 0) {
+        // (2n)!!
+        let pow = 1;
+        while (num / Math.pow(2, pow) >= 1) {
+          // how many even numbers inside
+          two += Math.floor(num / Math.pow(2, pow));
+          pow++;
+        }
+        pow = 1;
+        while (num / Math.pow(10, pow) >= 1) {
+          // how many zeros inside
+          zeros += Math.floor(num / Math.pow(10, pow));
+          pow++;
+        }
+        pow = 1;
+        while (num / Math.pow(50, pow) >= 1) {
+          // how many zeros inside
+          zeros += Math.ceil(Math.floor(num / Math.pow(50, pow)) / 2);
+          pow++;
+        }
+      } else {
+        // (2n+1)!!
+        let pow = 1;
+        while (num / Math.pow(5, pow) >= 1) {
+          // how many 5s inside
+          five += Math.ceil(Math.floor(num / Math.pow(5, pow)) / 2);
+          pow++;
+        }
+      }
+    } else {
+      // n!
+      let num = parseInt(arr[i]);
+      let pow = 1;
+      while (num / Math.pow(2, pow) >= 1) {
+        // how many even numbers inside
+        two += Math.floor(num / Math.pow(2, pow));
+        pow++;
+      }
+      pow = 1;
+      while (num / Math.pow(5, pow) >= 1) {
+        // how many zeros (5*2) inside
+        zeros += Math.floor(num / Math.pow(5, pow));
+        pow++;
+        two -= Math.floor(num / Math.pow(5, pow));
+      }
+    }
+  }
+  return zeros + Math.min(five, two);
+};
